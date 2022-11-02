8a4e5a0a9038661c99528cb536b6fe9f725fcd69
Francoise Dasilver <francisse@Francoises-MacBook-Pro.local>
2022-11-02 00:09:01 -0400

    adding new lives code to my git hub

---
+++ compare_three_numbers.py
@@ -0,0 +1,11 @@
+# Write a Python program that prints "Equal" if three numbers a, b,and c are equal.
+#If at least one number if different, the program should print "Not Equal"
+a = int(input("enter a number\n"))
+b = int(input("enter a number\n"))
+c = int(input("enter a number\n"))
+
+if a==b==c:
+    print("equal")
+else:
+    print('not equal')
+
---
+++ control_flow.py
@@ -0,0 +1,10 @@
+
+#Write a Python program that prints if a number num is either Positive or Negative or Zero
+
+number = int(input("Enter a number: "))
+if number > 0:
+   print("Positive number")
+elif number == 0:
+   print("Zero")
+else:
+   print("Negative number")
