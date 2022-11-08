436b4ee6d9dac42a2f8c0fb7754624c207a9755c
Francoise Dasilver <francisse@Francoises-MacBook-Pro.local>
2022-11-07 21:56:31 -0500

    add new lives code to my git hub

---
+++ max_and_min_value.py
@@ -0,0 +1,13 @@
+#This code will write a python program that print the largest and smallest number in a list using the max and min method.
+#the two value will be print in the same line and the largest will be first, then the smallest.
+
+positive_number=[2,4,6,8,10,12,14,16]
+negative_number=[-2,-4,-5,-7,-3,-1,]
+int=[]
+
+print (max(positive_number) , min(positive_number))
+print (max(negative_number) , min(negative_number))
+if len(int) == 0:
+    print("none")
+
+#print (min([3, 5, 9, 1, -5]))
--- sum_calculator.py
+++ sum_calculator.py
@@ -4,6 +4,6 @@ var_2 = int(input("give me an even number\n"))
 
 #sum = var_1 + var_2
 
-#print (f"The sum of {var_1} and {var_2} is: {sum}.")
+#print(f"The sum of {var_1} and {var_2} is: {sum}.")
 print (f"The sum of {var_1} and {var_2} is: {var_1} + {var_2}.")
 
